a38df30149bcd586427f9e131c301c6efda768ee
Simon.Jiang <simon.jiang@liferay.com>
2022-03-17 17:02:47 +0800

    LPS-148033 fix test error

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/BaseProjectTemplatesTestCase.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/BaseProjectTemplatesTestCase.java
@@ -1151,6 +1151,37 @@ public interface BaseProjectTemplatesTestCase {
 		}
 	}
 
+	public default File removeGradlePropertiesInWorkspace(
+			File workspaceDir, String gradleProperties)
+		throws IOException {
+
+		File gradlePropertiesFile = new File(workspaceDir, "gradle.properties");
+
+		List<String> oldGradlePropertiesList = Files.readAllLines(
+			gradlePropertiesFile.toPath());
+
+		StringBuilder newGradlePropertiesBuilder = new StringBuilder();
+
+		for (String property : oldGradlePropertiesList) {
+			if (property.equals(gradleProperties)) {
+				continue;
+			}
+
+			newGradlePropertiesBuilder.append(property);
+			newGradlePropertiesBuilder.append(System.lineSeparator());
+		}
+
+		String newGradlePropertiesContenet =
+			newGradlePropertiesBuilder.toString();
+
+		Files.write(
+			gradlePropertiesFile.toPath(),
+			newGradlePropertiesContenet.getBytes(),
+			StandardOpenOption.TRUNCATE_EXISTING);
+
+		return gradlePropertiesFile;
+	}
+
 	public default void replaceElementByName(
 			Document document, String oldElementName, String newElementName,
 			String textString)
